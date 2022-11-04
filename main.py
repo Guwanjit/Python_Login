92698e5540e5d9709bcf3db9b8d225cb1bf3fa75
Guwanjith <guwanjithsakbo13@gmail.com>
2022-11-04 14:26:15 +0530

    adding variables

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 root = Tk()
 root.geometry("500x300")
 
@@ -10,16 +11,24 @@ gender = Label(root, text="Gender :")
 emergency = Label(root, text="Emergency :")
 paymentmode = Label(root, text="Payment Mode :")
 
-name.grid(row= 1, column= 2)
-phone.grid(row= 2, column= 2)
-gender.grid(row= 3, column= 2)
-emergency.grid(row= 4, column= 2)
-paymentmode.grid(row= 5, column= 2)
+name.grid(row=1, column=2)
+phone.grid(row=2, column=2)
+gender.grid(row=3, column=2)
+emergency.grid(row=4, column=2)
+paymentmode.grid(row=5, column=2)
 
 namevalue = StringVar
 phonevalue = StringVar
 gendervalue = StringVar
 emergencyvalue = StringVar
 paymentmodevalue = StringVar
+
+checkvalue = IntVar
+
+nameentry = Entry(root, textvariable=namevalue)
+phoneentry = Entry(root, textvariable=phonevalue)
+genderentry = Entry(root, textvariable=gendervalue)
+emergencyentry = Entry(root, textvariable=emergencyvalue)
+paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
 
 root.mainloop()
